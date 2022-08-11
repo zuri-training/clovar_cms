3398b8675d0f4eddc05a583e428779fbca468504
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2022-08-11 13:47:44 +0100

    fix: complete landing page

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,18 +1,1 @@
 var typed=new Typed(".auto-type",{strings:["Contents","Concepts","Passion"],typeSpeed:125,backSpeed:150,loop:!0,cursorChar:""}),typed1=new Typed(".auto-type1",{strings:["Contents","Concepts","Passion"],typeSpeed:125,backSpeed:150,loop:!0,cursorChar:""});
-
-var faq = document.getElementsByClassName('faq-page');
-var i;
-for (i = 0; i < faq.length; i++) {
-  faq[i].addEventListener('click', function () {
-    /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-    this.classList.toggle('active');
-
-    var body = this.nextElementSibling;
-    if (body.style.display === 'block') {
-      body.style.display = 'none';
-    } else {
-      body.style.display = 'block';
-    }
-  });
-}
