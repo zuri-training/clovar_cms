6e0879826ab82b2df1f60d39c67ab1bc9c056a29
Prideland-Okoi <prideland.okoi@gmail.com>
2022-08-11 06:23:43 +0100

    inputted 'load static' on all html templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
 from .views import HomePageView, DashboardView
+#from .views import choicepick, signoutview, loginview, newpage, pickniche, signupview, verify_email
+
+app_name="pages"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    #path("choose/", choicepick.as_view(), name="choose"),
+    #path("choose/", choicepick, name="choose")
+    
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -10,3 +10,20 @@ class HomePageView(TemplateView):
 
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
+
+#class choicepick(TemplateView):
+    #template_name = "choose-url.html"
+#def choicepick(request):
+	#return render(request=request, template_name='choose-url.html')
+#def signoutview(request):
+	#return render(request=request, template_name='frontend/confirm-sign-out.html')
+#def loginview(request):
+	#return render(request=request, template_name='frontend/login.html')
+#def newpage(request):
+	#return render(request=request, template_name='frontend/new-page-desc.html')
+#def pickniche(request):
+	#return render(request=request, template_name='frontend/pick-niche.html')
+#def signupview(request):
+	#return render(request=request, template_name='frontend/signup-up.html')
+#def verify_email(request):
+	#return render(request=request, template_name='frontend/verify-email.html')
