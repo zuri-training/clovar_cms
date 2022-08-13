124ddfdfa48360c8e957e3262b6dc40bd818da31
chidiarua <chidiarua99@gmail.com>
2022-08-13 08:44:09 +0100

    more pages rendering and settings config

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import HomePageView, DashboardView, SelectTemplateView
+from .views import HomePageView, DashboardView, SelectTemplateView, BloggerUsersView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("dashboard/users/", BloggerUsersView.as_view(), name="users"),
     path("select_template/", SelectTemplateView.as_view(), name="select_template"),
 ]
