623736cad8afd8896f621235d83f872223b7ae63
Prideland-Okoi <prideland.okoi@gmail.com>
2022-08-20 05:50:00 +0100

    defaulttemplate and usertemplated created

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class DefaultTemplates(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=100, blank=True)
+    html=models.TextField()
+    #css=models.TextField()
+
+class CustomUserTemplates(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=100, blank=True)
+    html=models.TextField()
+    #css=models.TextField()
+    created_by=models.ForeignKey(CustomUser, related_name='CustomUserTemplates', on_delete=models.DO_NOTHING)
